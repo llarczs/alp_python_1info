7cd55264ee94d854fa48e8f4f998e0d6b4fca943
llarczs <laraxywww.17@gmail.com>
2026-06-26 09:23:29 -0300

    Add surname greeting logic in questao5.py

---
+++ questao5.py
@@ -0,0 +1,10 @@
+sobrenome = input("Digite o sobrenome: ")
+
+if sobrenome == "PINHEIRO" or sobrenome == "ARAÚJO":
+    print("Bom Dia Nação")
+
+elif sobrenome == "BONNER" or sobrenome == "VASCONCELOS":
+    print("Jornal Brasileiro")
+
+else:
+    print("Apresentador(a) desconhecido(a)")
