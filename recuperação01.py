bbd1bcf2173951172d03baffde0a408a68c3ba7d
llarczs <laraxywww.17@gmail.com>
2026-07-16 21:27:04 -0300

    Criar recuperação01.py

---
+++ recuperação01.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número inteiro: "))
+# Ajustado de 'numero' para 'num' para coincidir com a variável da linha anterior
+if num % 2 == 0:
+    print("o numero adicionado e par")
+else:
+    print("o número adicionado é impar")
