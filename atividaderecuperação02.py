b7de4b66b8af8b0205a46d2ab4f976b1ab38cd7f
llarczs <laraxywww.17@gmail.com>
2026-07-16 21:22:58 -0300

    Criar atividaderecuperação02.py

---
+++ atividaderecuperação02.py
@@ -0,0 +1,12 @@
+notal = float(input("digite a primeira nota"))
+nota2 = float(input("digite a segunda nota"))
+nota3 = float(input("digite a terceira nota"))
+media (nota1 + nota2 + nota3) /3
+ if media >=7
+print("aprovado")
+elif media >=4=
+print("prova final")
+
+else:
+
+print("reprovado")
