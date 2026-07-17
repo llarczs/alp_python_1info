187d5e76d412fce6b8452f5c5c167c6299e698df
llarczs <laraxywww.17@gmail.com>
2026-07-16 21:28:12 -0300

    Criar recuperação03.py

---
+++ recuperação03.py
@@ -0,0 +1,7 @@
+usuário = input("digite o nome do usuário: ")
+senha = input("digite a senha: ")
+
+if usuário == "aluno" and senha == "12345":
+    print("olá seja bem vindo")
+else:
+    print("acesso negado")
